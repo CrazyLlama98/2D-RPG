8e2d957e6adc2481f8ec9dc53f8b238d08bb0cf1
iAndu <alex.banu14@gmail.com>
2017-04-13 23:43:15 +0300

    Mob class added

---
+++ src/Mob.cpp
@@ -0,0 +1,14 @@
+#include "Mob.h"
+#define ENTITY_PARAMS b2World *_world, const oxygine::Vector2 &_pos, const float _scale
+
+Mob::Mob(const int _health, const int _damage, const int _xp, ENTITY_PARAMS) : Entity(_world, b2_staticBody, _pos, _scale) 
+{
+	SetHealth(_health);
+	SetDamage(_damage);
+	SetXp(_xp);
+}
+
+void Mob::DealDamage() { return; }
+void Mob::Respawn() { return; }
+void Mob::Die() { return; }
+void Mob::Click(oxygine::Event *_event) { return; }
