ed1d0bb44031256b7ed858d448513bdfb35720f8
iAndu <alex.banu14@gmail.com>
2017-05-03 20:42:53 +0300

    MainActor bug fixes

--- src/MainActor.cpp
+++ src/MainActor.cpp
@@ -1,10 +1,9 @@
-#pragma once
 #include "MainActor.h"
 
 MainActor::MainActor(): _world(0)
 {
     
-    setSize(getStage()->getSize());
+    setSize(oxygine::getStage()->getSize());
     
     _world = new b2World(b2Vec2(0, 10));
     
--- src/MainActor.h
+++ src/MainActor.h
@@ -1,12 +1,12 @@
 #pragma once
 #include "oxygine-framework.h"
+#include "Box2D/Box2D.h"
 
-class MainActor: public Actor
+class MainActor : public oxygine::Actor
 {
 public:
 
     b2World* _world;
     
     MainActor();
-
 };
