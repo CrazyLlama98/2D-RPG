bdb2f11b82ac2c66caa02099c2696dc4fe47d2d5
iAndu <alex.banu14@gmail.com>
2017-05-05 21:40:26 +0300

    Minor changes

--- src/MainActor.cpp
+++ src/MainActor.cpp
@@ -43,7 +43,7 @@ void MainActor::RandomSpawn()
 		do {
 			pos.x = rand() % (int)getSize().x;
 			pos.y = rand() % (int)getSize().y;
-		} while (pos.x < 70 || pos.x > getSize().x - 70 || pos.y < 70 || pos.y > getSize().y - 70); //|| overlaps());
+		} while (pos.x < 64 || pos.x > 1080 || pos.y < 64 || pos.y > 630); //|| overlaps());
 		spCharacter mob = new Character(100, 5, 20, mob_types[type], res::resources.getResAnim(mob_types[type] + "_spawn"), _world, pos, b2_staticBody, 1);
 		mob->addTween(TweenAnim(res::resources.getResAnim(mob_types[type] + "_spawn")), 700);
 		addChild(mob);
