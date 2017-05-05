8c2c62b1afff7aafddc77d546da67d4706032e8e
iAndu <alex.banu14@gmail.com>
2017-05-05 22:03:32 +0300

    No overlaps when spawning mobs. Removed smart pointers.

--- src/MainActor.cpp
+++ src/MainActor.cpp
@@ -7,6 +7,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <cstring>
+#include <cmath>
 
 using namespace oxygine;
 
@@ -20,6 +21,7 @@ MainActor::MainActor(): _world(0)
     _world = new b2World(b2Vec2(0, 10));   
 
 	hero = new Hero(100, 10, 0, 100, "hero", res::resources.getResAnim("hero_idle_up"), _world, getSize() / 2, 0.6);
+	_entities.push_back(hero);
 	addChild(hero);
 	this->addEventListener(TouchEvent::CLICK, CLOSURE(this, &MainActor::MoveHero));
 	RandomSpawn();
@@ -36,15 +38,17 @@ void MainActor::RandomSpawn()
 {
 	std::string mob_types[] = { "dwarf", "troll", "skeleton" };
 	srand(time(0));
-	for (int i = 0; i < 10; ++i)
+	for (int i = _entities.size(); i < 11; ++i)
 	{
 		int type = rand() % 3;
 		Vector2 pos;
 		do {
 			pos.x = rand() % (int)getSize().x;
 			pos.y = rand() % (int)getSize().y;
-		} while (pos.x < 64 || pos.x > 1080 || pos.y < 64 || pos.y > 630); //|| overlaps());
-		spCharacter mob = new Character(100, 5, 20, mob_types[type], res::resources.getResAnim(mob_types[type] + "_spawn"), _world, pos, b2_staticBody, 1);
+		} while (pos.x < 64 || pos.x > 1080 || pos.y < 64 || pos.y > 630 || Overlaps(pos));
+		Character *mob = new Character(100, 5, 20, mob_types[type], res::resources.getResAnim(mob_types[type] + "_spawn"),
+						 _world, pos, b2_staticBody, 1);
+		_entities.push_back(mob);
 		mob->addTween(TweenAnim(res::resources.getResAnim(mob_types[type] + "_spawn")), 700);
 		addChild(mob);
 	}
@@ -90,4 +94,15 @@ void MainActor::ClickCharacter(Event* _event)
             }
         }
     }
+}
+
+bool MainActor::Overlaps(const Vector2 _pos)
+{
+	for (auto it = _entities.begin(); it != _entities.end(); ++it)
+	{
+		Vector2 mob_pos = (*it)->getPosition();
+		if (sqrt((mob_pos.x - _pos.x) * (mob_pos.x - _pos.x) + (mob_pos.y - _pos.y) * (mob_pos.y - _pos.y)) < 200)
+			return true;
+	}
+	return false;
 }
