8617bed6f7626306ab8af5e49be60f0ad5e300ee
iAndu <alex.banu14@gmail.com>
2017-05-11 18:23:33 +0300

    Resources added

---
+++ proj.cmake/build/oxygine-sound-master/tremor/CMakeFiles/tremor.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/andu/oxygine/oxygine-sound-master/dependencies/tremor/Tremor/block.c" "/home/andu/Desktop/Facultate/Anul I/OOP/2D-RPG/proj.cmake/build/oxygine-sound-master/tremor/CMakeFiles/tremor.dir/Tremor/block.c.o"
+  "/home/andu/oxygine/oxygine-sound-master/dependencies/tremor/Tremor/codebook.c" "/home/andu/Desktop/Facultate/Anul I/OOP/2D-RPG/proj.cmake/build/oxygine-sound-master/tremor/CMakeFiles/tremor.dir/Tremor/codebook.c.o"
+  "/home/andu/oxygine/oxygine-sound-master/dependencies/tremor/Tremor/floor0.c" "/home/andu/Desktop/Facultate/Anul I/OOP/2D-RPG/proj.cmake/build/oxygine-sound-master/tremor/CMakeFiles/tremor.dir/Tremor/floor0.c.o"
+  "/home/andu/oxygine/oxygine-sound-master/dependencies/tremor/Tremor/floor1.c" "/home/andu/Desktop/Facultate/Anul I/OOP/2D-RPG/proj.cmake/build/oxygine-sound-master/tremor/CMakeFiles/tremor.dir/Tremor/floor1.c.o"
+  "/home/andu/oxygine/oxygine-sound-master/dependencies/tremor/Tremor/info.c" "/home/andu/Desktop/Facultate/Anul I/OOP/2D-RPG/proj.cmake/build/oxygine-sound-master/tremor/CMakeFiles/tremor.dir/Tremor/info.c.o"
+  "/home/andu/oxygine/oxygine-sound-master/dependencies/tremor/Tremor/mapping0.c" "/home/andu/Desktop/Facultate/Anul I/OOP/2D-RPG/proj.cmake/build/oxygine-sound-master/tremor/CMakeFiles/tremor.dir/Tremor/mapping0.c.o"
+  "/home/andu/oxygine/oxygine-sound-master/dependencies/tremor/Tremor/mdct.c" "/home/andu/Desktop/Facultate/Anul I/OOP/2D-RPG/proj.cmake/build/oxygine-sound-master/tremor/CMakeFiles/tremor.dir/Tremor/mdct.c.o"
+  "/home/andu/oxygine/oxygine-sound-master/dependencies/tremor/Tremor/registry.c" "/home/andu/Desktop/Facultate/Anul I/OOP/2D-RPG/proj.cmake/build/oxygine-sound-master/tremor/CMakeFiles/tremor.dir/Tremor/registry.c.o"
+  "/home/andu/oxygine/oxygine-sound-master/dependencies/tremor/Tremor/res012.c" "/home/andu/Desktop/Facultate/Anul I/OOP/2D-RPG/proj.cmake/build/oxygine-sound-master/tremor/CMakeFiles/tremor.dir/Tremor/res012.c.o"
+  "/home/andu/oxygine/oxygine-sound-master/dependencies/tremor/Tremor/sharedbook.c" "/home/andu/Desktop/Facultate/Anul I/OOP/2D-RPG/proj.cmake/build/oxygine-sound-master/tremor/CMakeFiles/tremor.dir/Tremor/sharedbook.c.o"
+  "/home/andu/oxygine/oxygine-sound-master/dependencies/tremor/Tremor/synthesis.c" "/home/andu/Desktop/Facultate/Anul I/OOP/2D-RPG/proj.cmake/build/oxygine-sound-master/tremor/CMakeFiles/tremor.dir/Tremor/synthesis.c.o"
+  "/home/andu/oxygine/oxygine-sound-master/dependencies/tremor/Tremor/vorbisfile.c" "/home/andu/Desktop/Facultate/Anul I/OOP/2D-RPG/proj.cmake/build/oxygine-sound-master/tremor/CMakeFiles/tremor.dir/Tremor/vorbisfile.c.o"
+  "/home/andu/oxygine/oxygine-sound-master/dependencies/tremor/Tremor/window.c" "/home/andu/Desktop/Facultate/Anul I/OOP/2D-RPG/proj.cmake/build/oxygine-sound-master/tremor/CMakeFiles/tremor.dir/Tremor/window.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/andu/oxygine/oxygine-sound-master/dependencies/tremor/../libogg/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ proj.cmake/build/oxygine-sound-master/tremor/CMakeFiles/tremor.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for tremor.
+# This may be replaced when dependencies are built.
